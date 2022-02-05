25d3ce092cab16afbed4728c9fa7855cb3053115
autumn-yng <92401509+autumn-yng@users.noreply.github.com>
2022-02-05 16:22:28 -0500

    Added files to repository

---
+++ unit1-movie/TableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewCell.swift
+//  unit1-movie
+//
+//  Created by Autumn Y Ngoc on 1/30/22.
+//
+
+import UIKit
+
+var movieArray = ["Autumn","Sharon"]
+class TableViewCell: UITableViewCell, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ unit1-movie/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  unit1-movie
+//
+//  Created by Autumn Y Ngoc on 1/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
